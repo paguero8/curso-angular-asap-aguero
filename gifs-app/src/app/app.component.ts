1e473b41c8a1fbf50589d4a1e8405eeece0a3c6c
paguero8 <paguero@asap-consulting.net>
2024-08-22 10:47:18 -0300

    styling sidebar with bootstrap

--- gifs-app/src/app/app.component.ts
+++ gifs-app/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { SearchbarComponent } from './components/searchbar/searchbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent],
+  imports: [RouterOutlet, SidebarComponent, SearchbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
